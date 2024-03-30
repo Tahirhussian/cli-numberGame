396c7ce1db01043e214fc0dd3283f8bb50ac23ec
Tahir Hussain <tahirhussainkhattak2758@gmail.com>
2024-03-30 15:57:30 +0500

    This is a cli number gussing game,

---
+++ index.js
@@ -0,0 +1,19 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const gusseNumber = await inquirer.prompt([
+    {
+        message: "gusse a number 1 to 10 :",
+        name: "userGussedNumber",
+        type: "number",
+    },
+]);
+let randonNumber = Math.floor(Math.random() * 10 + 1);
+if (gusseNumber.userGussedNumber === randonNumber) {
+    console.log("congratulation you gussed correct number ");
+}
+else if (gusseNumber.userGussedNumber > 10) {
+    console.log("please gusse number between 1 to 10");
+}
+else {
+    console.log("Try again!");
+}
